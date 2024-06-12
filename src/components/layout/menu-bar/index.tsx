d26810c5d963e03f1e3d380d2ad41127fddf8204
lixxkook00 <linhlinhkook00@gmail.com>
2024-06-13 00:03:09 +0700

    test : testing the first build on vercel

--- src/components/layout/menu-bar/index.tsx
+++ src/components/layout/menu-bar/index.tsx
@@ -1,7 +1,23 @@
+import './menu-bar.scss'
+
 const Menubar = () => {
   return (
   <div className="menu-bar">
-    
+    <div className="menu-bar-item">
+      Friends
+    </div>
+    <div className="menu-bar-item">
+    Mine
+    </div>
+    <div className="menu-bar-item">
+    Go
+    </div>
+    <div className="menu-bar-item">
+    Earn
+    </div>
+    <div className="menu-bar-item">
+    Airdrop
+    </div>
   </div>
   )
 }
