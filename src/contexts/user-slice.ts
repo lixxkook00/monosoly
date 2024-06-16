17f93411e6edc1dbe53f67b0dd767572f0e0fb5d
lixxkook00 <linhlinhkook00@gmail.com>
2024-06-16 09:45:08 +0700

    feat: update the amount store method

--- src/contexts/user-slice.ts
+++ src/contexts/user-slice.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface UserState {
   balance: number;
   energy: number;
+  position: number;
 }
 
 const initialState: UserState = {
   balance: 0,
   energy: 6,
+  position: 0,
 };
 
 export const userSlice = createSlice({
@@ -21,14 +23,17 @@ export const userSlice = createSlice({
       state.balance -= 1;
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.balance += action.payload;
+      state.balance = Math.max(0, state.balance + action.payload);
     },
     useEnergy: (state) => {
       state.energy -= 1;
     },
+    setPosition: (state, action: PayloadAction<number>) => {
+      state.position = action.payload;
+    }
   },
 });
 
-export const { increment, decrement, incrementByAmount, useEnergy } = userSlice.actions;
+export const { increment, decrement, incrementByAmount, useEnergy, setPosition } = userSlice.actions;
 
 export default userSlice.reducer;
