ef1aa877390ab2cf6e107ecb68b739a60b78fc1d
lixxkook00 <linhlinhkook00@gmail.com>
2024-06-17 22:59:50 +0700

    feat: added English Vietnamese Chinese Hindi Spanish French Bengali Standard Russian

--- src/pages/settings/components/language-popup.tsx
+++ src/pages/settings/components/language-popup.tsx
@@ -24,6 +24,12 @@ const LanguagePopup: React.FC = () => {
     { code: 'en', imgSrc: '/images/ic-en.png' },
     { code: 'vi', imgSrc: '/images/ic-vi.png' },
     { code: 'zh', imgSrc: '/images/ic-zh.png' },
+    { code: 'hi', imgSrc: '/images/ic-hi.png' },
+    { code: 'es', imgSrc: '/images/ic-es.png' },
+    { code: 'fr', imgSrc: '/images/ic-fr.png' },
+    { code: 'bn', imgSrc: '/images/ic-bn.png' },
+    { code: 'ar', imgSrc: '/images/ic-ar.png' },
+    { code: 'ru', imgSrc: '/images/ic-ru.png' },
   ];
 
   return (
@@ -34,7 +40,7 @@ const LanguagePopup: React.FC = () => {
           className="w-full flex items-center justify-between border-slate-700 border-b"
           onClick={() => handleChangeLanguage(lang.code)}
         >
-          <div className="text-lg font-semibold mt-4 pb-3">
+          <div className="text-base font-semibold mt-4 pb-3">
             {t(lang.code)}
           </div>
           <img className="w-9 object-contain" src={lang.imgSrc} alt={t(lang.code)} />
